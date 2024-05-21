eba3fcd693e202b9101093e4c9bcba98ece99a5a
Shevakula Naveen <135953626+divya613@users.noreply.github.com>
2024-05-21 13:44:56 +0530

    version 1

--- src/BudgetTracker.jsx
+++ src/BudgetTracker.jsx
@@ -5,20 +5,25 @@ import ExpenseList from "./components/ExpenseList"
 import TotalBudget from "./components/TotalBudget"
 import './styles.css'
 const BudgetTracker = () => {
+    //using useState hook to declare a totalBudget state varibale and function to update statevaribale
     const [totalBudget, setTotalBudget] = useState(0)
+    //using usestate hook to declare expenses state varibale and defalut value of it is empty array which keeps track of all expenses
     const [expenses, setExpenses] = useState([])
 
 
+    //function to add new exxpenses into our expenses array
     const addExpense = (expense) => {
         setExpenses([...expenses, expense])
     }
 
+    //function to remove expense from expenses array
     const removeExpense = (index) => {
         const updatedExpenses = [...expenses]
         updatedExpenses.splice(index, 1)
         setExpenses(updatedExpenses)
     }
 
+    //function that calculates balance amount from totalBudget when any expense is added
     const calBalance = () => {
         const totalExpense = expenses.reduce((total, expense) => total + expense.amount, 0)
         return totalBudget - totalExpense;
